31f270c4ae72ec7db72fd11b758450c1659224d2
Andrey <agolishin@inbox.ru>
2024-11-13 23:17:05 +0300

    project

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,6 @@ const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
         <App />
-      </Provider>
-    </BrowserRouter>
   </React.StrictMode>
 );
